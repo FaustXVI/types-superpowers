ca35291c5bf9fa9cf013a6d6d8892e88628d01c5
FaustXVI <xavier.detant@gmail.com>
2019-05-09 18:03:10 +0200

    Types as tests

--- src/address.ts
+++ src/address.ts
@@ -1,13 +1,13 @@
-function format(zipcode, city) {
+function format(zipcode: string, city: string) {
     return `${zipcode} ${city}`;
 }
 
 function validate(city: string, zipcode: string) {
     return !/[0-9]/.test(city)
-        &&  /^[0-9]{5}$/.test(zipcode);
+        && /^[0-9]{5}$/.test(zipcode);
 }
 
-function toHtml(city, zipcode) {
+function toHtml(city: string, zipcode: string) {
     if (validate(city, zipcode)) {
         return `<p>${format(city, zipcode)}</p>`;
     }
