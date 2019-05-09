a2c737d3b7bb9f715e0d2c104cf1e2fa501fdf52
FaustXVI <xavier.detant@gmail.com>
2019-05-09 18:24:05 +0200

    Types as fool-proof system (or is it not enough?)

--- src/address.ts
+++ src/address.ts
@@ -1,4 +1,28 @@
-function format(zipcode: string, city: string): string {
+class ZipCode {
+    private readonly value: string;
+
+    constructor(value: string) {
+        this.value = value;
+    }
+
+    public toString(): string {
+        return this.value;
+    }
+}
+
+class City {
+    private readonly value: string;
+
+    constructor(value: string) {
+        this.value = value;
+    }
+
+    public toString(): string {
+        return this.value;
+    }
+}
+
+function format(zipcode: ZipCode, city: City): string {
     return `${zipcode} ${city}`;
 }
 
@@ -9,7 +33,7 @@ function validate(city: string, zipcode: string): boolean {
 
 function toHtml(city: string, zipcode: string): string {
     if (validate(city, zipcode)) {
-        return `<p>${format(city, zipcode)}</p>`;
+        return `<p>${format(new ZipCode(city), new City(zipcode))}</p>`;
     }
     return "";
 }
