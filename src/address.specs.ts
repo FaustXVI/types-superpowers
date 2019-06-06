1552dd279225d38db4b3871977475383ff16c2fa
FaustXVI <xavier.detant@gmail.com>
2019-06-06 15:13:49 +0200

    Types as tests

--- src/address.specs.ts
+++ src/address.specs.ts
@@ -16,7 +16,6 @@ describe("An address", () => {
                 "Paris 13",
                 "P4ris",
                 null,
-                {},
             ]) {
 
                 it("because of city (" + invalidCity + ")", () => {
@@ -28,7 +27,6 @@ describe("An address", () => {
                 "7501",
                 "750190",
                 "75O19",
-                12345, // 02134
             ]) {
 
                 it("because of zipcode (" + invalidZipCode + ")", () => {
